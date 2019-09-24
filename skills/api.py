a87633a4d0ebc2f678bac636d0e36f93e8d69afc
skillpub <53432676+skillpub@users.noreply.github.com>
2019-09-24 18:53:23 +0300

    Update api.py

--- skills/api.py
+++ skills/api.py
@@ -1,6 +1,7 @@
 ```uses web API```
 from urllib.request import urlopen
 from urllib.error import HTTPError
+import json
 
 try:
     data = urlopen("http://api.open-notify.org/astros.json").read().decode()
