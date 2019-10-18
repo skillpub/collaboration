b460509998b797e08a15e7d71d25b6d4892fa46c
skillpub <53432676+skillpub@users.noreply.github.com>
2019-10-18 11:38:50 +0300

    Update grafa.py

--- skills/grafa.py
+++ skills/grafa.py
@@ -5,9 +5,11 @@ from PIL import Image
 panel_url = "http://127.0.0.1:3000/render/d-solo/XXXXXX/demo?orgId=1&panelId={}&width=1000&height=500"
 api_key = "Authorization: Bearer eyJrXXXXXXXXXXXXXXXXXXXXXXXXXXXXiOjF9"
 cookie = "Cookie: grafana_session=5384XXXXXXXXXXXXXXeae"
-auth_header = api_key # or cookie, whatever will work with your Grafana installation
 
-for panelId in ['1','2']:  
+auth_header = api_key # or cookie, whatever will work with your Grafana installation
+panels = ['1','2'] # place here panels ids you want to get  
+
+for panelId in panels:  
     file_tmp = "panel_{}.png".format(str(random.randint(1,10000)))
     os.system("curl -o {} '{}' -H '{}'".format(file_tmp, panel_url.format(panelId), auth_header))
 
