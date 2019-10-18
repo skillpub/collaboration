38234dcf79521f6902c5161c70d466bb697bf0dc
skillpub <53432676+skillpub@users.noreply.github.com>
2019-10-18 10:46:58 +0300

    Update grafa.py

--- skills/grafa.py
+++ skills/grafa.py
@@ -1,34 +1,17 @@
-'''grafana dashboards'''
-
+'''Grafana dashboards'''
 import os, random
 from PIL import Image
 
-api_key = "eyJrIjoiXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXIiwiaWQiOjF9"
-render_url = "http://10.10.10.10:3000/render/d-solo"
+panel_url = "http://10.10.10.10:3000/render/d-solo/XXXXXX/demo?orgId=1&panelId={}&width=1000&height=500"
+api_key = "Authorization: Bearer eyJrXXXXXXXXXXXXXXXXXXXXXXXXXXXXiOjF9"
+cookie = "Cookie: grafana_session=5384XXXXXXXXXXXXXXeae"
+auth_header = api_key # or cookie, whatever will work with your Grafana installation
 
-dashboards = {
-    'mydashboard' : ['/TXSTREZ/mydashboard\?orgId\=1\&panelId\=4\&width\=1000\&height\=500\&tz\=Europe%2FMoscow',
-                     '/TXSTREZ/mydashboard\?orgId\=1\&panelId\=5\&width\=1000\&height\=500\&tz\=Europe%2FMoscow'
-                    ],
-    'streaming' : ['/VYrfz82Wz/streaming\?orgId\=1\&panelId\=2\&width\=1000\&height\=500\&tz\=Europe%2FMoscow']
-}
+for panelId in ['1','2']:  
+    file_tmp = "panel_{}.png".format(str(random.randint(1,10000)))
+    os.system("curl -o {} '{}' -H '{}'".format(file_tmp, panel_url.format(panelId), auth_header))
 
-commands = ["mydashboard", "streaming"]
-argv = ' '.join(sys.argv)
+    image = Image.open(file_tmp)
+    image.show()
 
-command = next((command for command in commands if command in argv), None)
-
-if command is None:
-    print({'buttons':['grafa mydashboard', 'grafa streaming']})
-
-if command == 'mydashboard':
-    for panel in dashboards['mydashboard']:
-        
-        file_tmp = '/home/chernikovanv/nasahelper/tmp/grafa_graph_{}.png'.format(str(random.randint(1,10000)))
-        cmd = 'curl -o {} -H "Authorization: Bearer {}" {}{}'.format(file_tmp, api_key, render_url, panel)
-        os.system(cmd)
-
-        image = Image.open(file_tmp)
-        image.show()
-
-        os.remove(file_tmp)
+    os.remove(file_tmp)
